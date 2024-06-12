bfb7aa8bba695dfc9603cdf1cbbcb1044b7a8879
Saravanakumar Arumugam <saravanakumar.a.o@gmail.com>
2024-06-12 10:10:11 +0530

    Hide scroller before init

--- resources/views/components/scroll-text.blade.php
+++ resources/views/components/scroll-text.blade.php
@@ -1,5 +1,5 @@
 @if ($texts->count())
-    <div class="container-fluid">
+    <div class="container-fluid scroller" style="display: none">
         <div class="marquee py-2 w-100">
             @foreach ($texts as $text)
                 <div><span>{{ $text->scroll_text }}</span></div>
@@ -13,6 +13,8 @@
     <script src="{{ asset('assets/js/ticker.min.js') }}"></script>
     <script>
         $(function() {
+            $('.scroller').css('display', 'block')
+                .css('font-weight', 'bold');
             $('.marquee').ticker();
         });
     </script>
