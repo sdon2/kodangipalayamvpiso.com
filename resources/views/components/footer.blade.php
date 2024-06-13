d448438d3d24776c1ffacf3d249ee6e66e840627
Saravanakumar Arumugam <saravanakumar.a.o@gmail.com>
2024-06-13 12:29:30 +0530

    Changed footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,8 +3,11 @@
     <footer class="pt-3" style="color: #ffffff">
         <div class="container text-center pb-4">
             <div class="col-12">
-                Contents Owned and Updated by {{ config('app.name') }}<br><br>
-                Designed &amp; Developed by SVP Infotech, Tiruppur.
+                <small>
+                    Contents Owned and Updated by {{ config('app.name') }}<br>
+                    Designed &amp; Developed by SVP Infotech, Tiruppur.<br>
+                    Last updated on {{ \Carbon\Carbon::parse(\App\Models\Page::latest('updated_at')->first()->updated_at)->format('d/m/Y h:i A') }}<br>
+                </small>
             </div>
         </div>
     </footer>
